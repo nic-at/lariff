97d690da9b3c29b3cee977b9301cf5ee2edea854
Mario Öhlschläger <mario.oehlschlaeger@nic.at>
2016-05-04 13:38:55 +0200

    update update-shariif.sh

--- update-shariff.sh
+++ update-shariff.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/heiseonline/shariff-backend-php.git
-mv shariff-backend-php/src/* src/.
-rm ZendCache.php
-rm -r shariff-backend-php/
+mv shariff-backend-php/src/* src/Shariff/.
+rm src/Shariff/ZendCache.php
 rm -rf shariff-backend-php/
 
